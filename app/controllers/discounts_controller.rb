a3ccec781b5f86f6fd1ed9317b2d562bbbb9c760
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-04-24 15:53:11 -0600

    add pages and routs for discount features

---
+++ app/controllers/discounts_controller.rb
@@ -0,0 +1,12 @@
+class DiscountsController < ApplicationController
+  def index
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  get '/merchant/:merchant_id/discounts/new', to: 'discounts#new'
+  get '/merchant/:merchant_id/discounts', to: 'discounts#index'
+
+
   resources :merchant, only: [:show] do
     resources :dashboard, only: [:index]
     resources :items, except: [:destroy]
@@ -13,4 +19,5 @@ Rails.application.routes.draw do
     resources :merchant_status, only: [:update]
     resources :invoices, except: [:new, :destroy]
   end
+
 end
